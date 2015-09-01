e48ee620571606a755c5748aa03336dcabf11f7c
sc630@kent.ac.uk <sc630@kent.ac.uk>
2015-09-01 23:41:45 +0100

    started the project

---
+++ ODBCInfoGatherer/ODBCInfoGatherer/ConnectionManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace ODBCInfoGatherer
+{
+    class ConnectionManager
+    {
+        public static string SYSTEM_REGISTRY_PATH = @"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\ODBC\ODBC.INI";
+        public static string USER_REGISTRY_PATH = @"HKEY_CURRENT_USER\Software\ODBC\ODBC.INI";
+        
+        public ConnectionManager()
+        {
+
+        }
+
+        public List<ODBC> GetSystemDSNList64()
+        {
+            try
+            {
+                RegistryKey key = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            }
+            catch (NullReferenceException ex)
+            {
+
+            }
+            return null;
+        }
+
+        public List<ODBC> GetSystemDSNList32()
+        {
+            try
+            {
+                RegistryKey key = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            }
+            catch (NullReferenceException ex)
+            {
+
+            }
+            return null;
+        }
+
+        public List<ODBC> GetUserDSNList64()
+        {
+            try
+            {
+                RegistryKey key = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            }
+            catch(NullReferenceException ex)
+            {
+
+            }
+            return null;
+        }
+
+        public List<ODBC> GetUserDSNList32()
+        {
+            try
+            {
+                RegistryKey key = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            }
+            catch (NullReferenceException ex)
+            {
+
+            }
+            return null;
+        }
+
+    }
+}
---
+++ ODBCInfoGatherer/ODBCInfoGatherer/ODBC.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODBCInfoGatherer
+{
+
+
+    public class ODBC
+    {
+        private string Name {get; set;}
+        private string Driver {get; set;}
+        private string Server {get; set;}
+
+        public ODBC(string name, string driver, string server)
+        {
+            Name = name;
+            Driver = driver;
+            Server = server;
+        }
+
+
+
+
+    }
+}
