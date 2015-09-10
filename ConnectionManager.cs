d7e563e923ce55043a398eec2578528679fcc1de
sc630@kent.ac.uk <sc630@kent.ac.uk>
2015-09-10 20:48:35 +0100

    All functionality now there, and 99% commented.

--- ConnectionManager.cs
+++ ConnectionManager.cs
@@ -9,18 +9,28 @@ namespace ODBCInfoGatherer
 {
     public class ConnectionManager
     {
+        //A constant which stores the path to the odbc.ini registry
         public static string SYSTEM_REGISTRY_PATH = @"SOFTWARE\ODBC\ODBC.INI";
-        public static string USER_REGISTRY_PATH = @"Software\ODBC\ODBC.INI";
+        //A constant which stores the path to the root of the Local Machine registry, used for System DSN
         public static string LOCAL_MACHINE_STRING = @"HKEY_LOCAL_MACHINE";
+        //A constant which stores the path to the root of the Current User registry, used for User DSN
         public static string CURRENT_USER_STRING = @"HKEY_CURRENT_USER";
 
+        /// <summary>
+        /// Constructor for the ConnectionManager object, completely empty,
+        /// this class could probably be static.
+        /// </summary>
         public ConnectionManager()
         {
 
         }
 
       
-
+        /// <summary>
+        /// A method that gets a list of all the 64 bit System data sources
+        /// configured in the 64 bit ODBC.
+        /// </summary>
+        /// <returns>A list of ODBC objects</returns>
         public List<ODBC> GetSystemDSNList64()
         {
 
@@ -28,23 +38,52 @@ namespace ODBCInfoGatherer
             
         }
 
+        /// <summary>
+        /// A method that gets a list of all the 32 bit System data sources
+        /// configured in the 32 bit ODBC.
+        /// </summary>
+        /// <returns>A list of ODBC objects</returns>
         public List<ODBC> GetSystemDSNList32()
         {
             return GetSubKeysAndIterateOver(true, false, SYSTEM_REGISTRY_PATH);
             
         }
 
+        /// <summary>
+        /// A method that gets a list of all the 64 bit User data sources
+        /// configured in the 64 bit ODBC.
+        /// </summary>
+        /// <returns>A list of ODBC objects</returns>
         public List<ODBC> GetUserDSNList64()
         {
             return GetSubKeysAndIterateOver(false, true, SYSTEM_REGISTRY_PATH);
             
         }
 
+        /// <summary>
+        /// A method that gets a list of all the 32 bit User data sources
+        /// configured in the 32 bit ODBC.
+        /// </summary>
+        /// <returns>A list of ODBC objects</returns>
         public List<ODBC> GetUserDSNList32()
         {
             return GetSubKeysAndIterateOver(true, true, SYSTEM_REGISTRY_PATH);
         }
-
+        
+        /// <summary>
+        /// A method that gets all the sub keys in the registry representing
+        /// ODBC connections, and calls GetDataAndConstructODBC() to
+        /// construct the ODBC object for each of these, then adds
+        /// them to a List and returns this.
+        /// </summary>
+        /// <param name="use32BitView">Whether to look in the 32 bit registry or 64 bit registry, 
+        /// if true then it looks in the 32 bit registry, otherwise 64</param>
+        /// <param name="lookInCurrentUser">Whether to look in the Current_User registry,
+        /// or the local_machine registry, if true it looks in Current_User, otherwise
+        /// Local_Machine, controls whether it's returning user data sources or system data sources
+        /// </param>
+        /// <param name="path">The registry path to look in.</param>
+        /// <returns>A List of ODBC objects which represent the various ODBC connections.</returns>
         private List<ODBC> GetSubKeysAndIterateOver(bool use32BitView, bool lookInCurrentUser, string path)
         {
             List<ODBC> listOfConnections = new List<ODBC>();
--- ODBC.cs
+++ ODBC.cs
@@ -10,10 +10,19 @@ namespace ODBCInfoGatherer
 
     public class ODBC
     {
+        //The Data source name of the ODBC connection.
         public string Name {get; private set;}
+        //The driver of the ODBC connection.
         public string Driver {get; private set;}
+        //The server of the ODBC connection.
         public string Server {get; private set;}
 
+        /// <summary>
+        /// Constructor for ODBC objects.
+        /// </summary>
+        /// <param name="name">The Data source name of the ODBC connection.</param>
+        /// <param name="driver">The Driver of the ODBC connection.</param>
+        /// <param name="server">The server of the ODBC connection.</param>
         public ODBC(string name, string driver, string server)
         {
             Name = name;
